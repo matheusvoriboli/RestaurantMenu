ff2fd69aab94c2d0ced420dc4eeaa6d66cccf591
Matheus Riboli <CX-Matheus.Riboli@glhomes.com>
2023-12-22 12:44:12 -0300

    feature: Services getting api data implemented

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,19 @@
+'use client';
+import { getRestaurantDetails } from "@/services/restaurantService";
 import Link from "next/link";
+import { useEffect } from "react";
 
 export default function Home() {
+
+  useEffect(() => {
+    getInitialRestaurantDetails();
+  })
+
+  const getInitialRestaurantDetails = async () => {
+    const restaurantDetails = await getRestaurantDetails();
+    console.log(restaurantDetails);
+  };
+
   return (
     <div>
       <h1>Home</h1>
