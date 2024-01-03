7e03392c8395052cb01fdf0abe16d8c32fbdbcd5
Matheus Riboli <CX-Matheus.Riboli@glhomes.com>
2024-01-03 16:59:27 -0300

    feature: Changed en to en-US

--- src/services/i18n.ts
+++ src/services/i18n.ts
@@ -1,19 +1,19 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
-import enTranslations from '../locales/en/translations.json';
-import ptBRTranslations from '../locales/pt/translations.json';
+import enTranslations from '../locales/enUs/translations.json';
+import ptBRTranslations from '../locales/ptBr/translations.json';
 
 i18n.use(initReactI18next).init({
   resources: {
-    en: {
+    "en-US": {
       translation: enTranslations,
     },
     "pt-BR": {
       translation: ptBRTranslations,
     },
   },
-  lng: "en",
-  fallbackLng: "en",
+  lng: "en-US",
+  fallbackLng: "en-US",
   interpolation: {
     escapeValue: false,
   },
